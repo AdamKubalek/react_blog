1a37df62867d744d3f5b28de00b91e0b9d979e3a
Adam Kubalek <adam.kubalek@email.cz>
2023-01-31 12:10:29 +0100

    Added custom hook

--- src/Missing.js
+++ src/Missing.js
@@ -1,10 +1,15 @@
+import { Link } from "react-router-dom";
+
 const Missing = () => {
-    return (
-      <main>
-        <h1>Missing</h1>
-      </main>
-    );
-  };
-  
-  export default Missing;
-  
+  return (
+    <main className="Missing">
+      <h2>Page Not Found</h2>
+      <p>Well, that's disappointing.</p>
+      <p>
+        <Link to="/">Go Home</Link>
+      </p>
+    </main>
+  );
+};
+
+export default Missing;
--- src/NewPost.js
+++ src/NewPost.js
@@ -1,10 +1,33 @@
-const NewPost = () => {
-    return (
-      <main>
-        <h1>NewPost</h1>
-      </main>
-    );
-  };
-  
-  export default NewPost;
-  
+const NewPost = ({
+  handleSubmit,
+  postTitle,
+  setPostTitle,
+  postBody,
+  setPostBody,
+}) => {
+  return (
+    <main className="NewPost">
+      <h2>NewPost</h2>
+      <form className="newPostForm" onSubmit={handleSubmit}>
+        <label htmlFor="title">Title:</label>
+        <input
+          type="text"
+          id="postTitle"
+          value={postTitle}
+          required
+          onChange={(e) => setPostTitle(e.target.value)}
+        />
+        <label htmlFor="body">Post:</label>
+        <textarea
+          id="postBody"
+          value={postBody}
+          onChange={(e) => setPostBody(e.target.value)}
+          required
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </main>
+  );
+};
+
+export default NewPost;
